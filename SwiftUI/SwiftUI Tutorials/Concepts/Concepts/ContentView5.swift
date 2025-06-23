f5fde42e2ec9db4ad46e1cf6f826143a23227de6
澄界 <hisnameissky@gmail.com>
2025-06-24 07:07:58 +0800

    Add files via upload

---
+++ SwiftUI/SwiftUI Tutorials/Concepts/Concepts/ContentView5.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView5.swift
+//  Concepts
+//
+//  Created by 澄界 on 2025/06/17.
+//
+
+import SwiftUI
+
+struct ContentView5: View {
+    var body: some View {
+        Image("Yellow_Daisy")
+            .resizable()
+            .scaledToFit()
+            
+    }
+}
+
+#Preview {
+    ContentView5()
+}
