868e0a3b5a3da61169c2f4e5f55ebe682e8d7dd3
澄界 <hisnameissky@gmail.com>
2025-07-31 00:47:46 +0800

    Add files via upload

---
+++ SwiftUI/CalcApp/CalcApp/CalcAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  CalcAppApp.swift
+//  CalcApp
+//
+//  Created by 澄界 on 2025/06/29.
+//
+
+import SwiftUI
+
+@main
+struct CalcAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
